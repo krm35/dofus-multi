6b580f9db7c37e45dd3ed396bcc8abab24915902
krm35 <etcc@protonmail.com>
2024-03-06 15:58:52 +0100

    Update launcher version

--- back/constants.js
+++ back/constants.js
@@ -30,7 +30,7 @@ if (!languages.includes(language)) {
 
 module.exports = {
     port: 0,
-    version: "3.12.9",
+    version: "3.12.10",
     isTest: process.argv.includes("dev=true"),
     logs: this.isTest || !fs.existsSync("./logs"),
     uid: fs.existsSync(uidPath) ? fs.readFileSync(uidPath).toString() : null,
