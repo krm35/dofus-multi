0ef66c1b40428e9cec9dcb152b941ac1dbd353dc
krm35 <etcc@protonmail.com>
2024-04-06 13:01:22 +0200

    Use path.join

--- back/index.js
+++ back/index.js
@@ -30,10 +30,8 @@ require('./launcher');
 
 if (process.platform === "win32") {
     const tempPath = path.join(process.env.LOCALAPPDATA, 'Temp');
-    fs.existsSync(tempPath) && fs.readdirSync(tempPath).filter(f => f.includes("frida")).forEach(f => fs.rmSync(tempPath + "\\" + f, {
-        recursive: true,
-        force: true
-    }));
+    fs.existsSync(tempPath) && fs.readdirSync(tempPath).filter(f => f.includes("frida"))
+        .forEach(f => fs.rmSync(path.join(tempPath, f), {recursive: true, force: true}));
 }
 
 const server = http.createServer(
@@ -73,7 +71,7 @@ wss['on']('connection', function connection(ws) {
 
 let newVersion = null;
 
-if (!process.argv.includes("dev=true")) {
+if (!c.isTest) {
     (async () => {
         const [, version] = await request(
             {
